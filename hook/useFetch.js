f167f138f8ed5f1fcf7117a18e38296d0727273a
rafael-mingossi <rafaelmingossi@gmail.com>
2023-05-15 07:11:53 +1000

    first commit

---
+++ hook/useFetch.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: "https://jsearch.p.rapidapi.com/search",
+    params: {
+      query: "Python developer in Texas, USA",
+      page: "1",
+      num_pages: "1",
+    },
+    headers: {
+      "X-RapidAPI-Key": "c680d2ddabmsh84d9167c6748786p1f401fjsn82c5cb17e43b",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+};
