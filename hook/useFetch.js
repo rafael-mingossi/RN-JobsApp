83fcfd365dc3d65c9b593f5356818d07ad886dff
rafael-mingossi <rafaelmingossi@gmail.com>
2023-05-15 19:32:05 +1000

    routes and new components

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,22 +1,48 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useFetch = () => {
+const key = process.env.RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const options = {
     method: "GET",
-    url: "https://jsearch.p.rapidapi.com/search",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     params: {
-      query: "Python developer in Texas, USA",
-      page: "1",
-      num_pages: "1",
+      ...query,
     },
     headers: {
       "X-RapidAPI-Key": "c680d2ddabmsh84d9167c6748786p1f401fjsn82c5cb17e43b",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
   };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (e) {
+      console.log("catch", e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData().then((r) => console.log("fetchD ->>", r));
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData().then((r) => console.log("REfecthD -->>", r));
+  };
+
+  return { data, isLoading, error, refetch };
 };
+export default useFetch;
